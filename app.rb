15bcf9510fdd009cbc44dc19f1186d2640bd5748
GurlWithCurlz <gurlwithcurlz@gmail.com>
2020-04-09 20:58:59 +0100

    Added gif functionality

--- app.rb
+++ app.rb
@@ -14,6 +14,12 @@ post '/happy' do
     params[:user_name]+',you are spreading the love!'
 end
 
+post '/happy-gif' do
+  status 200
+  post_happy_gif params[:text]
+  params[:user_name]+',you like to giphy!'
+end
+
 def post_tq message
 
   slack_webhook = ENV['SLACK_WEBHOOK_URL']
@@ -34,5 +40,17 @@ def post_happy message
   {"text" => message,
    "username" => "HappinessBot"}.to_json,
     headers: {'content-type'=>'application/json'}
+
+end
+
+def post_happy_gif message
+
+  slack_webhook = ENV['SLACK_WEBHOOK_URL']
+  giphy_api_key = 8x96A5YlCJRCqplr4gjULJW13sLtY6FV
+  gif_url = "http://api.giphy.com/v1/gifs/search?q="+ message+ "&api_key=" + giphy_api_key + "&limit=5"
+
+  response = HTTParty.get(gif_url), headers: {"Accept" => "application/json" })
+
+  HTTParty.post slack_webhook, response.body
 
 end

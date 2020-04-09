f0f13da1a0779f8b58ad40ffd1aa4ccb2316edf6
Selina S. Solomon <gurlwithcurlz@gmail.com>
2020-04-09 15:09:23 +0100

    Added functionality

--- app.rb
+++ app.rb
@@ -2,19 +2,35 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 
-post '/anonymize' do
+post '/thanks' do
     status 200
-    post_tq params[:text], params[:channel_id]
+    post_tq params[:text]
 end
 
-def post_tq message, channel_id
+post '/happy' do
+    status 200
+    post_happy params[:text]
+end
+
+def post_tq message
+
+  slack_webhook = ENV['SLACK_WEBHOOK_URL']
+
+  HTTParty.post slack_webhook, body:
+  {"text" => 'thanks '+ message,
+   "username" => "HappinessBot"}.to_json,
+    headers: {'content-type'=>'application/json'}
+
+end
+
+
+def post_happy message
 
   slack_webhook = ENV['SLACK_WEBHOOK_URL']
 
   HTTParty.post slack_webhook, body:
   {"text" => message.to_s,
-   "username" => "HappinessBot",
-   "channel" => params[:channel_id]}.to_json,
+   "username" => "HappinessBot"}.to_json,
     headers: {'content-type'=>'application/json'}
 
 end

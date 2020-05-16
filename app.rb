5a66eec610be26401f3217d1b6734c5644172f83
Selina S. Solomon <gurlwithcurlz@gmail.com>
2020-05-16 18:52:33 +0100

    Gif testing

--- app.rb
+++ app.rb
@@ -52,8 +52,7 @@ def post_happy_gif message
   # payload = response.parsed_response
 
   HTTParty.post slack_webhook, body:
-  {"text" => message,
-  "image_url" => response["data"]["images"]["downsized"]["url"],
+  {"text" => message + response["data"]["images"]["downsized"]["url"] + "Powered by Giphy", 
    "username" => "HappinessBot"}.to_json,
     headers: {'content-type'=>'application/json'}
 

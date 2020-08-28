ddb46ce22505ee5542daad2808689aab2f6514b6
Selina S. Solomon <gurlwithcurlz@gmail.com>
2020-08-28 16:28:43 +0100

    Hackday PL 28082020

--- app.rb
+++ app.rb
@@ -160,7 +160,7 @@ def post_happy_gif_test response_url, message
 
 end
 
-def post_happy_gif_test_response payload
+def post_happy_gif_test_response payload_json
 
   slack_webhook = ENV['TEST_WEBHOOK_URL']
   # message = payload["type"]
@@ -199,7 +199,7 @@ def post_happy_gif_test_response payload
   #               headers: {'content-type' => 'application/json'}
 
   HTTParty.post slack_webhook, body:
-  {"text" => payload[:type],
+  {"text" => payload_json[type],
     "username" => "HappinessBot"}.to_json,
     headers: {'content-type'=>'application/json'}
 

4099dd1b48dc9ae0ae816f0bdb8c1b29773e43e9
Selina S. Solomon <gurlwithcurlz@gmail.com>
2021-01-27 15:29:00 +0000

    Hackday PL 27012021

--- app.rb
+++ app.rb
@@ -211,7 +211,7 @@ def post_happy_gif_test_response payload
   #               headers: {'content-type' => 'application/json'}
 
   HTTParty.post slack_webhook, body:
-  {"text" => payload[:type],
+  {"text" => payload,
     "username" => "HappinessBot"}.to_json,
     headers: {'content-type'=>'application/json'}
 

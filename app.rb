fb0668024255696c7d053b2d7e12abe26fed3d6d
Selina S. Solomon <gurlwithcurlz@gmail.com>
2021-01-27 14:21:48 +0000

    Hackday PL 27012021

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ post '/happygif-test' do
   params[:user_name]+"..just fyi, Selina is really happy I'm now interactive!"
 end
 
-post '/happy_gif_test_response' do
+post '/happygif-test-response' do
   status 200
   post_happy_gif_test_response params[:payload]
   ""
@@ -90,10 +90,14 @@ HTTParty.post slack_webhook,
 end
 
 def post_happy_gif_test response_url, message
+
+  # Get gif from Giphy API
   slack_webhook = response_url
   giphy_api_key = ENV['GIPHY_API_KEY']
   gif_url = "https://api.giphy.com/v1/gifs/random?api_key=" + giphy_api_key + "&tag="+ message+ "&rating=G"
   response = HTTParty.get(gif_url)
+
+  ## Setup dialogue box
 
   # Text block
   text_info = {"type"=>"plain_text", "text"=>"Are you happy with this gif?"}
@@ -109,7 +113,7 @@ def post_happy_gif_test response_url, message
     "type" => "button",
     "text" => button_text_yes,
     "action_id" => "gif_yes_button",
-    "value" => "gif_yes"
+    "value" => response["data"]["images"]["downsized"]["url"]
   }
 
   button_text_no = {
@@ -161,53 +165,53 @@ end
 def post_happy_gif_test_response payload
 
   slack_webhook = ENV['TEST_WEBHOOK_URL']
-  puts payload
-  payload_type = payload.is_a?(Hash)
-  puts "Payload is hash = " + payload_type.to_s
-  payload_type = payload.is_a? String
-  puts "Payload is string = " + payload_type.to_s
-  JSON.parse(payload)
-  puts payload
-  payload_type = payload.is_a?(Hash)
-  puts payload[:type]
+  # puts payload
+  # payload_type = payload.is_a?(Hash)
+  # puts "Payload is hash = " + payload_type.to_s
+  # payload_type = payload.is_a? String
+  # puts "Payload is string = " + payload_type.to_s
+  # JSON.parse(payload)
+  # puts payload
+  # payload_type = payload.is_a?(Hash) # I think the issue is that JSON can't parse the [ ] brackets in the string...
+  # # puts payload[:type]
 
-  # message = payload[:actions][:action_id]
+  message = payload[:actions][:action_id]
 
-  # actions = payload[:actions]
-  # if actions[:action_id] == gif_no_button # User didn't like gif
-  #   return
-  # gif_url = actions[:value]
-  #
-  #
-  #
-  # # Image block
-  # image_title = {"type" => "plain_text",
-  #                 "text" =>" Powered by Giphy"}
-  #
-  # image_block = {"type"=>"image",
-  #   "image_url"=>gif_url,
-  #   "alt_text"=>'test',
-  #   "title"=>image_title}
-  #
-  # # Text block
-  # text_info = {"type"=>"plain_text", "text"=>'test'}
-  # text_block = {"type"=>"section", "text"=>text_info}
-  #
-  # # Combine blocks
-  # blocks=[]
-  # blocks << text_block
-  # blocks << image_block
-  #
-  # params_hash={}
-  # params_hash[:blocks]=blocks
+  actions = payload[:actions]
+  if actions[:action_id] == 'gif_no_button' # User didn't like gif
+    return
+  gif_url = actions[:value]
 
-  # HTTParty.post slack_webhook,
-  #               body:params_hash.to_json,
-  #               headers: {'content-type' => 'application/json'}
 
-  HTTParty.post slack_webhook, body:
-  {"text" => "why does this work?" ,
-    "username" => "HappinessBot"}.to_json,
-    headers: {'content-type'=>'application/json'}
+
+  # Image block
+  image_title = {"type" => "plain_text",
+                  "text" =>" Powered by Giphy"}
+
+  image_block = {"type"=>"image",
+    "image_url"=>gif_url,
+    "alt_text"=>'test',
+    "title"=>image_title}
+
+  # Text block
+  text_info = {"type"=>"plain_text", "text"=>'test'}
+  text_block = {"type"=>"section", "text"=>text_info}
+
+  # Combine blocks
+  blocks=[]
+  blocks << text_block
+  blocks << image_block
+
+  params_hash={}
+  params_hash[:blocks]=blocks
+
+  HTTParty.post slack_webhook,
+                body:params_hash.to_json,
+                headers: {'content-type' => 'application/json'}
+
+  # HTTParty.post slack_webhook, body:
+  # {"text" => "why does this work?" ,
+  #   "username" => "HappinessBot"}.to_json,
+  #   headers: {'content-type'=>'application/json'}
 
 end

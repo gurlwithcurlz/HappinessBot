e05d5823c2a8e0306a2247c7e6c6aff4f58c946e
Selina S. Solomon <gurlwithcurlz@gmail.com>
2021-04-14 15:32:01 +0100

    yes - we got there

--- app.rb
+++ app.rb
@@ -14,21 +14,21 @@ post '/happy' do
   params[:user_name]+',you are spreading the love!'
 end
 
+# post '/happygif' do
+#   status 200
+#   post_happy_gif params[:text]
+#   params[:user_name]+',you like to giphy!'
+# end
+
 post '/happygif' do
   status 200
-  post_happy_gif params[:text]
-  params[:user_name]+',you like to giphy!'
-end
-
-post '/happygif-test' do
-  status 200
-  post_happy_gif_test params[:response_url], params[:text]
+  post_happy_gif params[:response_url], params[:text]
   params[:user_name]+".. Selina is ecstatic that I'm now interactive!!!"
 end
 
 post '/happygif_test_response' do
   status 200
-  post_happy_gif_test_response params[:payload]
+  post_happy_gif_response params[:payload]
   ""
 end
 
@@ -55,41 +55,41 @@ def post_happy message
 
 end
 
-def post_happy_gif message
+# def post_happy_gif message
+#
+#   slack_webhook = ENV['SLACK_WEBHOOK_URL']
+#   giphy_api_key = '8x96A5YlCJRCqplr4gjULJW13sLtY6FV'
+#   gif_url = "https://api.giphy.com/v1/gifs/random?api_key=" + giphy_api_key + "&tag="+ message+ "&rating=G"
+#   response = HTTParty.get(gif_url)
+#
+#   # Image block
+#   image_title = {"type" => "plain_text",
+#                   "text" => response["data"]["title"] + " Powered by Giphy"}
+#
+#   image_block = {"type"=>"image",
+#     "image_url"=>response["data"]["images"]["downsized"]["url"],
+#     "alt_text"=>message,
+#     "title"=>image_title}
+#
+#   # Text block
+#   text_info = {"type"=>"plain_text", "text"=>message}
+#   text_block = {"type"=>"section", "text"=>text_info}
+#
+#   # Combine blocks
+#   blocks=[]
+#   blocks << text_block
+#   blocks << image_block
+#
+#   params_hash={}
+#   params_hash[:blocks]=blocks
+#
+#   HTTParty.post slack_webhook,
+#                 body:params_hash.to_json,
+#                 headers: {'content-type' => 'application/json'}
+#
+# end
 
-  slack_webhook = ENV['SLACK_WEBHOOK_URL']
-  giphy_api_key = '8x96A5YlCJRCqplr4gjULJW13sLtY6FV'
-  gif_url = "https://api.giphy.com/v1/gifs/random?api_key=" + giphy_api_key + "&tag="+ message+ "&rating=G"
-  response = HTTParty.get(gif_url)
-
-  # Image block
-  image_title = {"type" => "plain_text",
-                  "text" => response["data"]["title"] + " Powered by Giphy"}
-
-  image_block = {"type"=>"image",
-    "image_url"=>response["data"]["images"]["downsized"]["url"],
-    "alt_text"=>message,
-    "title"=>image_title}
-
-  # Text block
-  text_info = {"type"=>"plain_text", "text"=>message}
-  text_block = {"type"=>"section", "text"=>text_info}
-
-  # Combine blocks
-  blocks=[]
-  blocks << text_block
-  blocks << image_block
-
-  params_hash={}
-  params_hash[:blocks]=blocks
-
-  HTTParty.post slack_webhook,
-                body:params_hash.to_json,
-                headers: {'content-type' => 'application/json'}
-
-end
-
-def post_happy_gif_test response_url, message
+def post_happy_gif response_url, message
 
   # Get gif from Giphy API
   slack_webhook = response_url
@@ -177,9 +177,9 @@ def post_happy_gif_test response_url, message
 
 end
 
-def post_happy_gif_test_response payload
+def post_happy_gif_response payload
 
-  slack_webhook = ENV['TEST_WEBHOOK_URL']
+  slack_webhook = ENV['SLACK_WEBHOOK_URL']
   payload = JSON.parse(payload)
   puts payload.to_s
   # Construct HappinessBot message
